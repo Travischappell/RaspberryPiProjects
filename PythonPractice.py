0a062c01190d8964f407a8b1543a5b86aacf8e79
Travischappell <travis.chappell0130@gmail.com>
2019-05-12 20:34:02 -0400

    Create PythonPractice.py

---
+++ PythonPractice.py
@@ -0,0 +1,13 @@
+import requests
+
+url = 'http://api.open-notify.org/astros.json'
+r = requests.get(url)
+data = r.json()
+names = [record['name'] for record in data]
+
+print(data)
+print(data['number'])
+print(data['people'])
+print(data['people'][0])
+print(data['people'][0]['name'])
+print(names)
