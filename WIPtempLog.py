fb1604b423155a46438af9ee73a5a12942bb2e88
Travischappell <travis.chappell0130@gmail.com>
2019-06-05 22:34:48 -0400

    Update WIPtempLog.py

--- WIPtempLog.py
+++ WIPtempLog.py
@@ -13,8 +13,9 @@
 
 
 import sys
-
+from time import sleep, strftime, time
 import Adafruit_DHT
+
 # Assign sensor type: [Adafruit_DHT.DHT11, Adafruit_DHT.DHT12, Adafruit_DHT.AM2302]
 sensor = Adafruit_DHT.DHT11
 pin = 4
@@ -36,18 +37,26 @@ else:
 """
 # Try to grab a sensor reading.  Use the read_retry method which will retry up
 # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
+def write_temp(temperature):
+    with open("/home/pi/Documents/Python/Script_Logs/roomtemplog.csv", "a") as log:
+        log.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(temperature)))
+
+iteration = 0
+
+while iteration < 5:
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 # Un-comment the line below to convert the temperature to Fahrenheit.
-temperature = temperature * 9/5.0 + 32
+    temperature = temperature * 9/5.0 + 32
+    write_temp(temperature)
+    if humidity is not None and temperature is not None:
+        print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+    else:
+        print('Failed to get reading. Try again!')
+    sleep 15
+    iteration += 1
 
 # Note that sometimes you won't get a reading and
 # the results will be null (because Linux can't
 # guarantee the timing of calls to read the sensor).
 # If this happens try again!
-if humidity is not None and temperature is not None:
-    print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
-else:
-    print('Failed to get reading. Try again!')
-    sys.exit(1)
-https://github.com/Travischappell/RaspberryPiProjects.git
